2c30096dc14ce066d110320c02c3dbb0098c1427
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2023-08-30 08:13:57 +0200

    Pull latest go-xcode (#37)
    * Update go-xcode
    
    * Add new files
    
    * Update main.go
    
    * Update go-xcode to v1.0.16
    
    * Update main.go

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"os"
 	"path"
 	"path/filepath"
@@ -110,7 +109,7 @@ func main() {
 	// Output files
 	rawXcodebuildOutputLogPath := filepath.Join(conf.OutputDir, "raw-xcodebuild-output.log")
 
-	tempDir, err := ioutil.TempDir("", "XCOutput")
+	tempDir, err := os.MkdirTemp("", "XCOutput")
 	if err != nil {
 		fail("Could not create result bundle path directory: %s", err)
 	}
@@ -137,19 +136,8 @@ func main() {
 	fmt.Println()
 	log.Infof("Analyzing the project")
 
-	isWorkspace := false
-	ext := filepath.Ext(absProjectPath)
-	if ext == ".xcodeproj" {
-		isWorkspace = false
-	} else if ext == ".xcworkspace" {
-		isWorkspace = true
-	} else {
-		fail("Project file extension should be .xcodeproj or .xcworkspace, but got: %s", ext)
-	}
+	analyzeCmd := xcodebuild.NewCommandBuilder(absProjectPath, "analyze")
 
-	analyzeCmd := xcodebuild.NewCommandBuilder(absProjectPath, isWorkspace, xcodebuild.AnalyzeAction)
-
-	analyzeCmd.SetDisableIndexWhileBuilding(conf.DisableIndexWhileBuilding)
 	analyzeCmd.SetScheme(conf.Scheme)
 
 	if conf.DisableCodesign {
@@ -161,9 +149,13 @@ func main() {
 		if customOptions, err = shellquote.Split(conf.XcodebuildOptions); err != nil {
 			fail("failed to shell split XcodebuildOptions (%s), error: %s", conf.XcodebuildOptions, err)
 		}
-
-		analyzeCmd.SetCustomOptions(customOptions)
 	}
+
+	if conf.DisableIndexWhileBuilding {
+		customOptions = append(customOptions, "COMPILER_INDEX_STORE_ENABLE=NO")
+	}
+
+	analyzeCmd.SetCustomOptions(customOptions)
 
 	if !sliceutil.IsStringInSlice("-resultBundlePath", customOptions) {
 		analyzeCmd.SetResultBundlePath(xcresultPath)
--- vendor/github.com/bitrise-io/go-xcode/xcodebuild/build.go
+++ vendor/github.com/bitrise-io/go-xcode/xcodebuild/build.go
@@ -1,11 +1,18 @@
 package xcodebuild
 
 import (
-	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
 
 	"github.com/bitrise-io/go-utils/command"
+)
+
+const (
+	// XCWorkspaceExtension ...
+	XCWorkspaceExtension = ".xcworkspace"
+	// XCProjExtension ...
+	XCProjExtension = ".xcodeproj"
 )
 
 /*
@@ -29,51 +36,32 @@ xcodebuild -workspace <workspacename> \
 	[<buildaction>]...
 */
 
-// const ...
-const (
-	ArchiveAction Action = "archiveAction"
-	BuildAction   Action = "buildAction"
-	AnalyzeAction Action = "analyzeAction"
-)
-
-// Action ...
-type Action string
-
 // CommandBuilder ...
 type CommandBuilder struct {
-	projectPath   string
-	isWorkspace   bool
-	scheme        string
-	configuration string
-	destination   string
-
-	// buildsetting
-	forceDevelopmentTeam              string
-	forceProvisioningProfileSpecifier string
-	forceProvisioningProfile          string
-	forceCodeSignIdentity             string
-	disableCodesign                   bool
-	disableIndexWhileBuilding         bool
-
-	// buildaction
-	customBuildActions []string
+	actions []string
 
 	// Options
+	projectPath      string
+	scheme           string
+	configuration    string
+	destination      string
+	xcconfigPath     string
+	authentication   *AuthenticationParams
 	archivePath      string
 	customOptions    []string
 	sdk              string
 	resultBundlePath string
+	testPlan         string
 
-	// Archive
-	action Action
+	// buildsetting
+	disableCodesign bool
 }
 
 // NewCommandBuilder ...
-func NewCommandBuilder(projectPath string, isWorkspace bool, action Action) *CommandBuilder {
+func NewCommandBuilder(projectPath string, actions ...string) *CommandBuilder {
 	return &CommandBuilder{
 		projectPath: projectPath,
-		isWorkspace: isWorkspace,
-		action:      action,
+		actions:     actions,
 	}
 }
 
@@ -83,45 +71,27 @@ func (c *CommandBuilder) SetScheme(scheme string) *CommandBuilder {
 	return c
 }
 
-// SetDestination ...
-func (c *CommandBuilder) SetDestination(destination string) *CommandBuilder {
-	c.destination = destination
-	return c
-}
-
 // SetConfiguration ...
 func (c *CommandBuilder) SetConfiguration(configuration string) *CommandBuilder {
 	c.configuration = configuration
 	return c
 }
 
-// SetForceDevelopmentTeam ...
-func (c *CommandBuilder) SetForceDevelopmentTeam(forceDevelopmentTeam string) *CommandBuilder {
-	c.forceDevelopmentTeam = forceDevelopmentTeam
+// SetDestination ...
+func (c *CommandBuilder) SetDestination(destination string) *CommandBuilder {
+	c.destination = destination
 	return c
 }
 
-// SetForceProvisioningProfileSpecifier ...
-func (c *CommandBuilder) SetForceProvisioningProfileSpecifier(forceProvisioningProfileSpecifier string) *CommandBuilder {
-	c.forceProvisioningProfileSpecifier = forceProvisioningProfileSpecifier
+// SetXCConfigPath ...
+func (c *CommandBuilder) SetXCConfigPath(xcconfigPath string) *CommandBuilder {
+	c.xcconfigPath = xcconfigPath
 	return c
 }
 
-// SetForceProvisioningProfile ...
-func (c *CommandBuilder) SetForceProvisioningProfile(forceProvisioningProfile string) *CommandBuilder {
-	c.forceProvisioningProfile = forceProvisioningProfile
-	return c
-}
-
-// SetForceCodeSignIdentity ...
-func (c *CommandBuilder) SetForceCodeSignIdentity(forceCodeSignIdentity string) *CommandBuilder {
-	c.forceCodeSignIdentity = forceCodeSignIdentity
-	return c
-}
-
-// SetCustomBuildAction ...
-func (c *CommandBuilder) SetCustomBuildAction(buildAction ...string) *CommandBuilder {
-	c.customBuildActions = buildAction
+// SetAuthentication ...
+func (c *CommandBuilder) SetAuthentication(authenticationParams AuthenticationParams) *CommandBuilder {
+	c.authentication = &authenticationParams
 	return c
 }
 
@@ -155,17 +125,18 @@ func (c *CommandBuilder) SetDisableCodesign(disable bool) *CommandBuilder {
 	return c
 }
 
-// SetDisableIndexWhileBuilding ...
-func (c *CommandBuilder) SetDisableIndexWhileBuilding(disable bool) *CommandBuilder {
-	c.disableIndexWhileBuilding = disable
+// SetTestPlan ...
+func (c *CommandBuilder) SetTestPlan(testPlan string) *CommandBuilder {
+	c.testPlan = testPlan
 	return c
 }
 
 func (c *CommandBuilder) cmdSlice() []string {
 	slice := []string{toolName}
+	slice = append(slice, c.actions...)
 
 	if c.projectPath != "" {
-		if c.isWorkspace {
+		if filepath.Ext(c.projectPath) == XCWorkspaceExtension {
 			slice = append(slice, "-workspace", c.projectPath)
 		} else {
 			slice = append(slice, "-project", c.projectPath)
@@ -180,49 +151,17 @@ func (c *CommandBuilder) cmdSlice() []string {
 		slice = append(slice, "-configuration", c.configuration)
 	}
 
-	if c.disableCodesign {
-		slice = append(slice, "CODE_SIGNING_ALLOWED=NO")
-	} else {
-		if c.forceDevelopmentTeam != "" {
-			slice = append(slice, fmt.Sprintf("DEVELOPMENT_TEAM=%s", c.forceDevelopmentTeam))
-		}
-
-		if c.forceProvisioningProfileSpecifier != "" {
-			slice = append(slice, fmt.Sprintf("PROVISIONING_PROFILE_SPECIFIER=%s", c.forceProvisioningProfileSpecifier))
-		}
-
-		if c.forceProvisioningProfile != "" {
-			slice = append(slice, fmt.Sprintf("PROVISIONING_PROFILE=%s", c.forceProvisioningProfile))
-		}
-
-		if c.forceCodeSignIdentity != "" {
-			slice = append(slice, fmt.Sprintf("CODE_SIGN_IDENTITY=%s", c.forceCodeSignIdentity))
-		}
-	}
-
 	if c.destination != "" {
 		// "-destination" "id=07933176-D03B-48D3-A853-0800707579E6" => (need the plus `"` marks between the `destination` and the `id`)
-		slice = append(slice, "-destination")
-		slice = append(slice, c.destination)
+		slice = append(slice, "-destination", c.destination)
 	}
 
-	if c.disableIndexWhileBuilding {
-		slice = append(slice, "COMPILER_INDEX_STORE_ENABLE=NO")
+	if c.xcconfigPath != "" {
+		slice = append(slice, "-xcconfig", c.xcconfigPath)
 	}
 
-	slice = append(slice, c.customBuildActions...)
-
-	switch c.action {
-	case ArchiveAction:
-		slice = append(slice, "archive")
-
-		if c.archivePath != "" {
-			slice = append(slice, "-archivePath", c.archivePath)
-		}
-	case BuildAction:
-		slice = append(slice, "build")
-	case AnalyzeAction:
-		slice = append(slice, "analyze")
+	if c.archivePath != "" {
+		slice = append(slice, "-archivePath", c.archivePath)
 	}
 
 	if c.sdk != "" {
@@ -231,6 +170,18 @@ func (c *CommandBuilder) cmdSlice() []string {
 
 	if c.resultBundlePath != "" {
 		slice = append(slice, "-resultBundlePath", c.resultBundlePath)
+	}
+
+	if c.authentication != nil {
+		slice = append(slice, c.authentication.args()...)
+	}
+
+	if c.testPlan != "" {
+		slice = append(slice, "-testPlan", c.testPlan)
+	}
+
+	if c.disableCodesign {
+		slice = append(slice, "CODE_SIGNING_ALLOWED=NO")
 	}
 
 	slice = append(slice, c.customOptions...)
